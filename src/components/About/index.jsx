255296d71fa92c1c2b9e4c67fd271b982015bb5a
cdwenz <cdwenz@gmail.com>
2023-05-07 22:20:41 -0300

    btn resume

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -7,9 +7,9 @@ function About() {
     <div className={styles.container}>
         <div className={styles.left}>
             <div className={styles.subtitle}>About me</div>
-            <h3>THIS IS THE WAY</h3>
+            <h3 className={styles.h3}>THIS IS THE WAY</h3>
             <div>Soy un apasionado de la tecnología y de su aplicación para superar desafíos ayudando a otras personas a simplificar su vida. Es por eso que uniendo mis bases en electrónica, experiencia en redes y helpdesk, sumado al mundo de la programación, hacen de mi trabajo, un placer. Soy observador, con gran capacidad de resolución de problemas y siempre dispuesto a aprender. Con experiencia en trabajo en equipo, liderando y acompañando.</div>
-            <input type='button' value='Download Resume' />
+            <input type='button' className={styles.btnResume} value='Download Resume' />
         </div>
         <div className={styles.right}> 
             <img src={img} alt='img' />
