e8a28952d69544fa0e719b42b910c4893b198dec
cdwenz <cdwenz@gmail.com>
2023-05-08 00:04:59 -0300

    added cards of projects (structure)

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,14 +1,55 @@
 import React from 'react';
 import img from '../../img/img_main.png';
+import img8 from '../../img/8.jpg';
+import img9 from '../../img/9.jpg';
+
 import styles from './project.module.css'
+
+const projects = [
+  {
+  title: 'titulo',
+  subtitle: 'subtitle',
+  description: 'descripción',
+  img: img
+  },
+  {
+  title: 'titulo2',
+  subtitle: 'subtitle2',
+  description: 'descripción2',
+  img: img
+  },
+  {
+  title: 'titulo3',
+  subtitle: 'subtitle3',
+  description: 'descripción3',
+  img: img
+  },
+  {
+  title: 'titulo3',
+  subtitle: 'subtitle3',
+  description: 'descripción3',
+  img: img
+  }
+]
 
 function Project() {
   return (
-    <div className={styles.container}>
-        <img src={img} alt="image" className={styles.imgProject}/>
-        <span className={styles.spanText1}>texto 1</span>
-        <span className={styles.spanText2}>texto 2</span>
+        <div className={styles.container}>
+          <div className={styles.grid}>
+    {projects.map((element, index)=>{
+      return (
+            <figure className={styles.effect_ming}  id={element.index}>
+              <img src={img9} alt="img09"/>
+              <figcaption>
+                <h2>{element.title} <span>{element.subtitle}</span></h2>
+                <p>{element.description}</p>
+                <a href="#">View more</a>
+              </figcaption>			
+            </figure>
+      )
+    })}
     </div>
+  </div>
   )
 }
 
