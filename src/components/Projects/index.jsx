827151a8fbf9916a39eb01655ce0896a95ca0aed
cdwenz <cdwenz@gmail.com>
2024-02-06 16:01:55 -0300

    feat: add proyects 0

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -7,34 +7,39 @@ import styles from './project.module.css'
 
 const projects = [
   {
-  title: 'titulo',
-  subtitle: 'subtitle',
-  description: 'descripción',
-  img: img
+  title: `Samuel D'ippolito`,
+  subtitle: 'Artista plástico',
+  description: 'Web de concepto',
+  img: "https://firebasestorage.googleapis.com/v0/b/images-cdwenz.appspot.com/o/SamDip%2Fexposiciones%2Fflores%2Fprincipios_de_conexion%2FLa%20Flor%20(El%20Abrazo)_1vert.jpg?alt=media&token=6ee6087e-5684-449c-a0b7-4371ae519c02",
+  url: "https://www.samueldippolito.com"
+},
+  {
+  title: 'Lanin CarWash',
+  subtitle: 'Lavadero',
+  description: 'Turnos web',
+  img: "https://firebasestorage.googleapis.com/v0/b/images-cdwenz.appspot.com/o/Portfolio%2Flanincarwash_org.png?alt=media&token=f5fa4ea0-2c45-4ef3-bba6-61c06207dbe4",
+  url: "https://www.lanincarwash.com"
   },
   {
-  title: 'titulo2',
-  subtitle: 'subtitle2',
-  description: 'descripción2',
-  img: img
+  title: 'Tierra prometida',
+  subtitle: 'Almacén natural',
+  description: 'ecommerce',
+  img: "https://firebasestorage.googleapis.com/v0/b/images-cdwenz.appspot.com/o/Portfolio%2Ftierraprometida.png?alt=media&token=0ed80145-1b81-4259-9f8f-1d76ad1bd242",
+  url: "https://ecommerce-eight-self.vercel.app/"
   },
   {
-  title: 'titulo3',
-  subtitle: 'subtitle3',
-  description: 'descripción3',
-  img: img
+  title: 'Naciones Unidas',
+  subtitle: 'Corralón',
+  description: 'ecommerce',
+  img: "https://firebasestorage.googleapis.com/v0/b/images-cdwenz.appspot.com/o/Portfolio%2Fcnu.png?alt=media&token=bd5f3b5e-6835-4a99-aecb-0c610de0796d",
+  url: ""
   },
   {
-  title: 'titulo4',
-  subtitle: 'subtitle3',
-  description: 'descripción3',
-  img: img
-  },
-  {
-  title: 'titulo5',
-  subtitle: 'subtitle3',
-  description: 'descripción3',
-  img: img
+  title: 'Landing Page',
+  subtitle: 'Neón',
+  description: 'css effects',
+  img: "https://firebasestorage.googleapis.com/v0/b/images-cdwenz.appspot.com/o/Portfolio%2Fneonlight.png?alt=media&token=b7ad0bc4-a04e-4b7c-830c-638b213df3fa",
+  url: "https://tradicional-house.vercel.app/"
   }
 ]
 
@@ -44,12 +49,13 @@ function Project() {
           <div className={styles.grid}>
             {projects.map((element, index)=>{
               return (
-                    <figure className={styles.effect_ming}  id={element.index}>
-                      <img src={img9} alt="img09"/>
+                    <figure key={index} className={styles.effect_ming}  id={element.index}>
+                      <img src={element.img} alt="img09"/>
                       <figcaption>
-                        <h2>{element.title} <span>{element.subtitle}</span></h2>
+                        <h2>{element.title}</h2>
+                        <span>{element.subtitle}</span>
                         <p>{element.description}</p>
-                        <a href="#">View more</a>
+                        {element.url ? <a href={element.url} target='_blank'>View more</a> : null}
                       </figcaption>			
                     </figure>
               )
